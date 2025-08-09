c2086c0906a9793e5697b059a7b4e5cb18c27d7e
Savaira-Chaudary <savairachaudarychaudary@gmail.com>
2025-08-09 12:54:21 +0500

    refactor register student and teacher

--- src/app/api/teacher/registerTeacher/route.js
+++ src/app/api/teacher/registerTeacher/route.js
@@ -6,6 +6,28 @@ export async function POST(request) {
     await connectDB();
 
     try {
+         // Get cookie from request
+        const cookieHeader = request.headers.get('cookie') || '';
+        const tokenMatch = cookieHeader.match(/admin_session=([^;]+)/);
+        const sessionToken = tokenMatch ? tokenMatch[1] : null;
+
+        if (!sessionToken) {
+            return NextResponse.json(
+                { success: false, message: "Unauthorized: No session token found in cookies." },
+                { status: 401 }
+            );
+        }
+
+        // Find admin with this session token
+        const admin = await Admin.findOne({ 'sessions.token': sessionToken });
+
+        if (!admin) {
+            return NextResponse.json(
+                { success: false, message: "Unauthorized: Invalid session." },
+                { status: 401 }
+            );
+        }
+
         const { email, password, fullName, phone, address, subjectSpecialization, qualification , teacherId} = await request.json();
 
         if (!email || !password || !fullName) {
