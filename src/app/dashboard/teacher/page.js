9c18baa73d5e81c41094e3fa7a5ce05948836a0c
Savaira-Chaudary <savairachaudarychaudary@gmail.com>
2025-08-18 10:35:13 +0500

    just a small change

--- src/app/dashboard/teacher/page.js
+++ src/app/dashboard/teacher/page.js
@@ -11,9 +11,9 @@ export default function TeacherDashboard() {
     router.replace("/login");
   }
 
+
   return (
     <div className="p-6">
-      <h1 className="text-xl font-bold">Teacher Dashboard</h1>
       <button
         onClick={handleLogout}
         className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
